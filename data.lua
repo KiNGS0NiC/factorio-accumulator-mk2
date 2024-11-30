0897e171081d6a40eb6803469b597eefe3b1f4eb
KiNG_S0NiC <mail@example.com>
2024-11-30 21:07:19 +0100

    Removed unnecessary table deepcopy

--- data.lua
+++ data.lua
@@ -1,34 +1,44 @@
 require("graphics")
 local item_sounds = require("__base__.prototypes.item_sounds")
 
-local accumulator_prototype = table.deepcopy(data.raw["accumulator"])
-accumulator_prototype.type = "accumulator"
-accumulator_prototype.name = "accumulator-mk2"
-accumulator_prototype.icon = "__accumulator-mk2__/graphics/icons/accumulator-mk2.png"
-accumulator_prototype.minable = {mining_time = 0.1, result = "accumulator-mk2"}
-accumulator_prototype.flags = {"placeable-neutral", "player-creation"}
-accumulator_prototype.fast_replaceable_group = "accumulator"
-accumulator_prototype.drawing_box_vertical_extension = 0.5
-accumulator_prototype.corpse = "accumulator-remnants"
-accumulator_prototype.dying_explosion = "accumulator-explosion"
-accumulator_prototype.max_health = 200
-accumulator_prototype.collision_box = {{-0.9, -0.9}, {0.9, 0.9}}
-accumulator_prototype.selection_box = {{-1, -1}, {1, 1}}
-accumulator_prototype.energy_source =
-{
-  type = "electric",
-  buffer_capacity = "50MJ",
-  usage_priority = "tertiary",
-  input_flow_limit = "900kW",
-  output_flow_limit = "900kW"
-}
-accumulator_prototype.chargable_graphics =
-{
-  picture = accumulator_mk2_picture(),
-  charge_animation = accumulator_mk2_charge(),
-  charge_cooldown = 30,
-  discharge_animation = accumulator_mk2_discharge(),
-  discharge_cooldown = 60
+local accumulator_prototype = {
+  type = "accumulator",
+  name = "accumulator-mk2",
+  icon = "__accumulator-mk2__/graphics/icons/accumulator-mk2.png",
+  minable = {
+    mining_time = 0.1,
+    result = "accumulator-mk2"
+  },
+  flags = {"placeable-neutral", "player-creation"},
+  fast_replaceable_group = "accumulator",
+  drawing_box_vertical_extension = 0.5,
+  corpse = "accumulator-remnants",
+  dying_explosion = "accumulator-explosion",
+  max_health = 200,
+  collision_box = {
+    {-0.9, -0.9},
+    {0.9, 0.9}
+  },
+  selection_box = {
+    {-1, -1},
+    {1, 1}
+  },
+  energy_source =
+  {
+    type = "electric",
+    buffer_capacity = "50MJ",
+    usage_priority = "tertiary",
+    input_flow_limit = "900kW",
+    output_flow_limit = "900kW"
+  },
+  chargable_graphics =
+  {
+    picture = accumulator_mk2_picture(),
+    charge_animation = accumulator_mk2_charge(),
+    charge_cooldown = 30,
+    discharge_animation = accumulator_mk2_discharge(),
+    discharge_cooldown = 60
+  }
 }
 
 local item = {
